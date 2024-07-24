9cef0fbbb52d6173243cc1da9cba23236ecc8a80
golohorzeus <golohorzeusufuoma@gmail.com>
2024-07-23 21:59:14 -0700

    Cleaned up project directory tree

--- config.py
+++
@@ -1,3 +0,0 @@
-DEFAULT_MESSAGE = "Welcome to this application console! Sport Information are all gotten from 'https://www.livescores.com/'"
-SHOW_MESSAGE = True  # Flag to control if message is displayed
-FILE1 = "./livescores/LiveScore_Football_Premier_League_Live_Football_Scores_Egypt.html"
